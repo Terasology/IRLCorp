f46c7aa90bc6f500829276fc1b7ed7d6f936e4eb
Nail Khanipov <foxflameless@gmail.com>
2021-08-26 00:22:20 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#40)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/irlCorp/components/ToolBlockPlacementComponent.java
+++ src/main/java/org/terasology/irlCorp/components/ToolBlockPlacementComponent.java
@@ -1,26 +1,19 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.irlCorp.components;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 
-public class ToolBlockPlacementComponent implements Component {
+public class ToolBlockPlacementComponent implements Component<ToolBlockPlacementComponent> {
     @Replicate
     public int maximumRange = 5;
     @Replicate
     public int maximumBlocks = 10;
+
+    @Override
+    public void copyFrom(ToolBlockPlacementComponent other) {
+        this.maximumBlocks = other.maximumBlocks;
+        this.maximumRange = other.maximumRange;
+    }
 }
